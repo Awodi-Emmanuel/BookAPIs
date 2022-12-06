a5be505fcee6cdd49955f530714612fe5a060499
Awodi-Emmanuel <Emmanuelawodi25@gmail.com>
2022-12-06 15:57:14 +0100

    Book models class added

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,1 +1,20 @@
-usin
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes
+
+namespace BookApis.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string BookName { get; set; } = null!;
+
+    public decimal Price { get; set; } 
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
